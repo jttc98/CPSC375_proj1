120905d7573dd08d9d8c46946adae48ed03ea968
jttc98 <jttc98@csu.fullerton.edu>
2021-04-05 20:42:45 -0700

    Added the skeleton code
    Adding the comments as well can be useful, since we need to write down the intsructions on how we perform data tidying with the csv tables and then obtain the resulting main table by joining the csv tables together.

---
+++ driver.R
@@ -0,0 +1,8 @@
+library(tidyverse) #Needed to perform data tidying operations
+
+#Make sure to use read_csv() when loading the data from the csv files.
+
+#For each csv file, perform the data tidying to ensure the table joining functions properly.
+
+#Now combine all of the tables together into one whole main table.
+#This main table will be the result obtained from data tidying and table joining.
