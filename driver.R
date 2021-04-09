b453c5f02516db281906afabfb0e5336565c3aa8
jttc98 <jttc98@csu.fullerton.edu>
2021-04-09 14:18:04 -0700

    Revised some syntax

--- driver.R
+++ driver.R
@@ -1,13 +1,13 @@
 library(tidyverse) #Needed to perform data tidying operations
 
-#Make sure to use read_csv() when loading the data from the csv files.
-#For each csv file, perform the data tidying to ensure the table joining functions properly.
+#First, read the csv files by using the read_csv() functions.
+#Then for each data, perform the data tidying operations so that the tables can be joined properly.
 Demographics <- read_csv("https://raw.githubusercontent.com/jttc98/CPSC375_proj1/main/demographics.csv")
 Demographics <- Demographics %>% replace(is.na(.), 0) #Replace the NAs with the zeros.
 #Rename the countries' names with a more suitable one.
 Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Korea, Dem. People's Rep.", "South Korea"))
-Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Korea, Rep..", "South Korea"))
-Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Iran, Islamic Rep..", "Iran"))
+Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Korea, Rep.", "South Korea"))
+Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Iran, Islamic Rep.", "Iran"))
 #Exclude the US from the Demographics table.
 Demographics <- Demographics %>% filter(Country != "United States")
 
