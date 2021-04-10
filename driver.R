4dee8b34ef5cbc618d0e79de30e7096587707b4b
jttc98 <jttc98@csu.fullerton.edu>
2021-04-10 09:37:35 -0700

    Added a few more columns to Demographics

--- driver.R
+++ driver.R
@@ -4,8 +4,9 @@ library(tidyverse) #Needed to perform data tidying operations
 #Then for each data, perform the data tidying operations so that the tables can be joined properly.
 Demographics <- read_csv("C:/Users/johnt/Documents/Data Science/demographics.csv")
 Demographics <- Demographics %>% select(`Country Name`, `Series Code`, `YR2015`)
+Demographics <- Demographics %>% pivot_wider(names_from = `Series Code`, values_from=`YR2015`)
 Demographics <- Demographics %>% replace(is.na(.), 0) #Replace the NAs with the zeros.
-Demographics <- Demographics %>% select(`Country` = `Country Name`)
+Demographics <- Demographics %>% select(`Country` = `Country Name`, `POP TOTAL` = `SP.POP.TOTL`, `POP URBAN` = `SP.URB.TOTL`)
 #Rename the countries' names to maintain the data consistency.
 Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Korea, Dem. People's Rep.", "South Korea"))
 Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Korea, Rep.", "South Korea"))
