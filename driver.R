3796b4c03c0d23745d4859a10feb8c9fb26a0c78
jttc98 <jttc98@csu.fullerton.edu>
2021-04-11 10:22:27 -0700

    Added the beds table
    I also combined the Demographics table together with the Beds table via inner_join. Only two tables left to go!

--- driver.R
+++ driver.R
@@ -2,17 +2,25 @@ library(tidyverse) #Needed to perform data tidying operations
 
 #First, read the csv files by using the read_csv() functions.
 #Then for each data, perform the data tidying operations so that the tables can be joined properly.
+#Rename the countries' names to maintain the data consistency, and exclude the US from the tables.
+Beds <- read_csv("C:/Users/johnt/Documents/Data Science/beds.csv")
+Beds <- Beds %>% filter(Year == 2015)
+Beds <- Beds %>% filter(Country != "United States of America")
+Beds <- Beds %>% select(`Country`, `Number of Beds` = `Hospital beds (per 10 000 population)`)
+Beds <- Beds %>% mutate(Country = replace(Country, Country == "Republic of Korea", "South Korea"))
+Beds <- Beds %>% mutate(Country = replace(Country, Country == "Iran (Islamic Republic of)", "Iran"))
+Beds <- Beds %>% mutate(Country = replace(Country, Country == "United Kingdom of Great Britain and Northern Ireland", "United Kingdom"))
+
 Demographics <- read_csv("C:/Users/johnt/Documents/Data Science/demographics.csv")
 Demographics <- Demographics %>% select(`Country Name`, `Series Code`, `YR2015`)
 Demographics <- Demographics %>% pivot_wider(names_from = `Series Code`, values_from=`YR2015`)
 Demographics <- Demographics %>% replace(is.na(.), 0) #Replace the NAs with the zeros.
 Demographics <- Demographics %>% select(`Country` = `Country Name`, `POP TOTAL` = `SP.POP.TOTL`, `POP URBAN` = `SP.URB.TOTL`)
-#Rename the countries' names to maintain the data consistency.
 Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Korea, Dem. People's Rep.", "South Korea"))
 Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Korea, Rep.", "South Korea"))
 Demographics <- Demographics %>% mutate(Country=replace(Country, Country=="Iran, Islamic Rep.", "Iran"))
-#Exclude the US from the Demographics table.
 Demographics <- Demographics %>% filter(Country != "United States")
 
 #Now combine all of the tables together into one whole main table.
+Demographics_AND_Beds <- Demographics %>% inner_join(Beds)
 #This main table will be the result obtained from data tidying and table joining.
